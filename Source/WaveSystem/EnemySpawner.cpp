315ea3a4789e271293d82ed32be0e994368cc4e3
Hunter <trahan.hunter@yahoo.com>
2021-01-29 13:36:12 -0600

    removed redundant code

--- Source/WaveSystem/EnemySpawner.cpp
+++ Source/WaveSystem/EnemySpawner.cpp
@@ -1,11 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "EnemySpawner.h"
 
-void AEnemySpawner::BeginPlay()
-{
-
-}
-
+///
+/// Calculate the enemy number for the next wave
+///
 void AEnemySpawner::calculateEnemyNumber()
 {
 	enemyNumber = (waveNumber * 5) - (waveNumber * 2);
--- Source/WaveSystem/EnemySpawner.h
+++ Source/WaveSystem/EnemySpawner.h
@@ -13,26 +13,45 @@ class WAVESYSTEM_API AEnemySpawner : public AGameModeBase
     GENERATED_BODY()
 
     protected:
-
-        //Called when the game starts or when spawned
+        ///
+        /// Called when the game starts or when spawned
+        ///
         virtual void BeginPlay();
 
       public:
 
+        ///
+        /// Set the number of enemies
+        ///
         UFUNCTION(BlueprintCallable)
         void setNumberofEnemies(int enemy) { enemyNumber = enemy; }
 
+        ///
+        /// Get the number of enemies
+        ///
         UFUNCTION(BlueprintCallable)
         int getEnemyAmount() { return enemyNumber; }
 
+        ///
+        /// Increse the wave number
+        ///
         UFUNCTION(BlueprintCallable)
         void newWaveNumber() { waveNumber++; }
 
+        ///
+        /// Increase the amount of enemies every round
+        ///
         UFUNCTION(BlueprintCallable)
         void calculateEnemyNumber();
 
     private:
 
+        ///
+        /// Enemy count
         int enemyNumber = 5;
+
+        ///
+        /// Wave number
+        ///
         int waveNumber = 1;
 };
